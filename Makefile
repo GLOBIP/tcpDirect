8b27533bad05dd8f493894e98e19caa51195ffb8
Kamil <GLOBIP>
2026-06-13 13:44:28 +0200

    SERWEREK

---
+++ Makefile
@@ -0,0 +1,17 @@
+SRCS=$(wildcard *.cpp)
+OBJS=$(SRCS:.cpp=.o)
+FLAGS = -I $(HOME)/boostasio/boost_1_82_0
+COMPILER = g++
+
+%.o: %.cpp
+	$(COMPILER) $(FLAGS) -c $< -o $@
+all: ${OBJS}
+	${COMPILER} ${OBJS} -o main ${FLAGS}
+
+
+run: all
+	./main
+
+.PHONY : clean
+clean :
+	-rm -f *.o $(OBJS)
---
+++ main.cpp
@@ -0,0 +1,54 @@
+
+#include "boost/asio/io_context.hpp"
+#include "boost/asio/ip/tcp.hpp"
+#include "boost/system/detail/error_code.hpp"
+#include <boost/asio.hpp>
+#include <boost/endian/arithmetic.hpp>
+#include <exception>
+#include <filesystem> // 1. Add the filesystem header
+#include <fstream>
+#include <iostream>
+#include <string>
+using boost::asio::ip::tcp;
+using std::filesystem::path;
+using NetworkSize = boost::endian::big_uint64_t;
+static std::vector<char> read_file(path const &fspec) {
+  std::ifstream file(fspec, std::ios::binary);
+  return std::vector<char>(std::istreambuf_iterator<char>(file), {});
+}
+
+void send_file_content_over_tcp(path const &fspec, tcp::socket &socket) {
+  auto const contents = read_file(fspec);
+  std::cout << "File read successfully. File size: " << contents.size()
+            << " bytes." << std::endl;
+
+  // Send the file content
+  NetworkSize fileSize = contents.size();
+
+  std::vector<boost::asio::const_buffer> payload{
+      boost::asio::buffer(&fileSize, sizeof(fileSize)),
+      boost::asio::buffer(contents),
+  };
+  boost::system::error_code ec;
+  auto n = write(socket, payload, ec);
+
+  std::cout << "Content bytes sent: " << n << " (" << ec.message() << ")\n";
+}
+
+int main() {
+  try {
+    boost::asio::io_context io;
+    tcp::acceptor acceptor(io, tcp::endpoint(tcp::v4(), 12234));
+    for (;;) {
+
+      tcp::socket socket(io);
+      acceptor.accept(socket);
+      send_file_content_over_tcp(
+          "/home/kamil/Dokumenty/ftpDSERVER/DATASENT/FILE_SENT.txt", socket);
+    }
+  } catch (std::exception &e) {
+    std::cerr << e.what() << '\n';
+  }
+
+  return 0;
+}
